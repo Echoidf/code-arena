83199a2fecb459370ce4c57cf37d10a2b3e5dced
zql <1241236275@qq.com>
2023-09-06 09:46:08 +0800

    update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 // 这在将应用部署到自定义域名或 CDN 上时特别有用，因为它允许您将静态资源存储在不同的位置
 let assetPrefix = ``
 // 用于为应用设置基础路径。这在将应用部署到子目录下时特别有用，因为它允许您指定应用所在的目录
-let basePath = `/`
+let basePath = ``
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
@@ -18,6 +18,7 @@ const nextConfig = {
   assetPrefix,
   basePath,
   reactStrictMode: true,
+  distDir: `.build`,
   images: {
     unoptimized: true,
   },
